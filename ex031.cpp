adae51f463ea8e693e019c2a8c3564616aec6885
806025582 <806025582@qq.com>
2017-03-14 16:41:01 +0800

    simple

---
+++ ex031.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	printf("%d,%d,%d\n",0123,0x123,123);
+	printf("%f,%f\n",123.4,1234e2);
+	return 0;
+}
---
+++ ex032.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	char a,b,c,d;
+	a='m';
+	b='r';
+	c='k';
+	d='j';
+	printf("%c%c%c%c\n",a,b,c,d);
+	return 0;
+}
