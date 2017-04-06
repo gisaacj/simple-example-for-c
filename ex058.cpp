5c4d505d4f7549febdd78546dfcb6880885ee5ca
806025582 <806025582@qq.com>
2017-04-06 23:12:15 +0800

    养老状态

---
+++ ex058.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int n,k=1,s=0,m,c=-1;
+	printf("½á¹ûÊÇ:");
+	for(n=11;n<100;n++){
+		k=1;
+		s=0;
+		m=n;
+		while(m){
+			k*=m%10;
+			s+=m%10;
+			m/=10;
+		}
+		if(k>s){
+			c++;
+			if(c%5==0)
+				printf("\n");
+			printf("%5d",n);
+		}
+	}
+	printf("\n");
+}
