a549b97e89b820b7c657e53f50c436b924d208ab
806025582 <806025582@qq.com>
2017-04-08 14:00:35 +0800

    .

---
+++ ex060.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	char c1,c2,c3;
+	c1=getche();
+	c2=getch();
+	c3=getchar();
+	putchar(c1);
+	putchar(c2);
+	putchar(c3);
+	return 0;
+}
---
+++ ex061.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float i;
+	scanf("%f",&i);
+	printf("\n%f",ceil(i));
+}
